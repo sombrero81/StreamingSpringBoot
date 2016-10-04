dd5d75a7662d330282d84718fbca06052c0f8fb5
sombrero81 <tead987@gmail.com>
2016-10-04 15:50:06 +0200

    04/oct/2016 16h00

---
+++ src/main/java/streaming/dao/PaysDAO.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import streaming.entity.Pays;
+
+/**
+ *
+ * @author admin
+ */
+
+@Repository
+public class PaysDAO {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    public Pays rechercher(long id){
+        
+        return em.find(Pays.class, id);
+    }
+    
+    @Transactional
+    public void ajouter(Pays p){
+        
+        em.persist(p);
+    }
+    
+    
+    
+}
